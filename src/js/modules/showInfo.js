0115b4e1b391b40a27a1f11559908d6794b5f8cf
Ramil <vk2011@bk.ru>
2021-08-18 13:10:20 +0500

    fix showInfo.js

--- src/js/modules/showInfo.js
+++ src/js/modules/showInfo.js
@@ -6,11 +6,13 @@ export default class ShowInfo {
     init() {
         this.btns.forEach(btn => {
             btn.addEventListener('click', function () {
-                this.sibling = btn.closest('.module__info-show').nextElementSibling;
+                try {
+                    this.sibling = btn.closest('.module__info-show').nextElementSibling;
 
-                this.sibling.classList.toggle('msg');
-                this.sibling.classList.add('animated', 'fadeIn');
-                this.sibling.style.marginTop = '20px';
+                    this.sibling.classList.toggle('msg');
+                    this.sibling.classList.add('animated', 'fadeIn');
+                    this.sibling.style.marginTop = '20px';
+                } catch(e){}
             });
         });
     }
